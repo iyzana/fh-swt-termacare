6d6e2e933fe1e1f0e1bc52092afa3302410dc277
kaiser <jannis.kaiser@adesso.de>
2017-04-12 15:04:54 +0200

    create javafx app

---
+++ src/main/java/de/adesso/termacare/App.java
@@ -0,0 +1,15 @@
+package de.adesso.termacare;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
