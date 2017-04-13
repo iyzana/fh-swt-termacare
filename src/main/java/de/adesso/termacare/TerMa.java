fa92b7614bf91424ba8d4eaaa42ad2d26908fa9d
5he1d0r <sebastian.linde@adesso.de>
2017-04-13 17:16:51 +0200

    rename App -> TerMa

--- src/main/java/de/adesso/termacare/TerMa.java
+++ src/main/java/de/adesso/termacare/TerMa.java
@@ -2,13 +2,11 @@ package de.adesso.termacare;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App extends Application {
+public class TerMa extends Application {
 
-    public static org.slf4j.Logger logger = LoggerFactory.getLogger(App.class);
+    public static org.slf4j.Logger logger = LoggerFactory.getLogger(TerMa.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -16,6 +14,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        Application.launch(App.class, args);
+        Application.launch(TerMa.class, args);
     }
 }
