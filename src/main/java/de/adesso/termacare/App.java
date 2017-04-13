5320658c729a25596f4c6d611bccba2be056c286
5he1d0r <sebastian.linde@adesso.de>
2017-04-13 17:15:51 +0200

    create logger in App

--- src/main/java/de/adesso/termacare/App.java
+++ src/main/java/de/adesso/termacare/App.java
@@ -2,8 +2,14 @@ package de.adesso.termacare;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App extends Application {
+
+    public static org.slf4j.Logger logger = LoggerFactory.getLogger(App.class);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
