e2ca53a28731aec7e3ef47868c382d4113c6959c
5he1d0r <sebastian.linde@adesso.de>
2017-04-14 21:32:11 +0200

    impl methods in AbstractController

--- src/main/java/de/adesso/termacare/gui/construct/AbstractController.java
+++ src/main/java/de/adesso/termacare/gui/construct/AbstractController.java
@@ -1,3 +1,19 @@
 package de.adesso.termacare.gui.construct;
 
-public abstract class AbstractController<T extends AbstractView> implements Controller {}
+import static de.adesso.termacare.TerMa.logger;
+
+public abstract class AbstractController<T extends AbstractView> implements Controller {
+
+	protected T view;
+
+	public void init(T view) {
+		this.view = view;
+		view.init();
+		logger.info("Initialised in: \"" + this.getClass().getSimpleName() + "\" the view: \"" + view.getClass().getSimpleName() + "\"");
+	}
+
+	@Override
+	public void show() {
+		view.show();
+	}
+}
