a3f515c291a04b0f1d18c0fb6ca9173045bca595
5he1d0r <sebastian.linde@adesso.de>
2017-04-14 21:16:30 +0200

    define methods in Controller

--- src/main/java/de/adesso/termacare/gui/construct/Controller.java
+++ src/main/java/de/adesso/termacare/gui/construct/Controller.java
@@ -1,3 +1,21 @@
 package de.adesso.termacare.gui.construct;
 
-interface Controller{}
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+interface Controller{
+
+	/**
+	 * Creates a new view with the stage.
+	 * Calls the <code>init()</code> on view.
+	 *
+	 * @param stage The stage. Given to view.
+	 * @param scene The scene. Given to view.
+	 */
+	void init(Stage stage, Scene scene);
+
+	/**
+	 * Calls <code>show()</code> on view
+	 */
+	void show();
+}
