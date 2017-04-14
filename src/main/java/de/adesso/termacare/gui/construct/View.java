e44ff3b12eb8331c8b01eb156d3b4693e518feef
5he1d0r <sebastian.linde@adesso.de>
2017-04-14 20:34:55 +0200

    define methods in View

--- src/main/java/de/adesso/termacare/gui/construct/View.java
+++ src/main/java/de/adesso/termacare/gui/construct/View.java
@@ -1,3 +1,52 @@
 package de.adesso.termacare.gui.construct;
 
-interface View{}
+import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+interface View{
+	/**
+	 * Here is the content defined witch is shown
+	 */
+	void init();
+
+	/**
+	 * Calls {@link Stage#show()}
+	 */
+	void show();
+
+	/**
+	 * The {@link #pictureForButton(Image, Button)}-method for all components
+	 */
+	void addPicturesToButtons();
+
+	/**
+	 * Adds the icon to the button and further text is under the picture
+	 * @param icon The picture for the button
+	 * @param button The button
+	 */
+	void pictureForButton(Image icon, Button button);
+
+	/**
+	 * The {@link #fillComponentWithText(Labeled, String)}-method for all components
+	 */
+	void fillComponentsWithSelectedLanguage();
+
+	/**
+	 * This Method fills the component with text
+	 * @param labeled The component
+	 * @param nameInBundle The identifier in resource-bundle
+	 */
+	void fillComponentWithText(Labeled labeled, String nameInBundle);
+
+	/**
+	 * This method register the listener for e.g. buttons, slider etc.
+	 */
+	void registerListener();
+
+	/**
+	 * Here gets a component its style-classes
+	 */
+	void setStyleClasses();
+}
