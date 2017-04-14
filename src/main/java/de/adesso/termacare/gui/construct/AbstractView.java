c07a5716fc10607debbce14e609048af043587bd
5he1d0r <sebastian.linde@adesso.de>
2017-04-14 21:29:38 +0200

    impl methods in AbstractView

--- src/main/java/de/adesso/termacare/gui/construct/AbstractView.java
+++ src/main/java/de/adesso/termacare/gui/construct/AbstractView.java
@@ -1,3 +1,46 @@
 package de.adesso.termacare.gui.construct;
 
-public abstract class AbstractView<T extends AbstractController> implements View {}
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Labeled;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public abstract class AbstractView<T extends AbstractController> implements View {
+
+	protected T controller;
+	protected Stage stage;
+	protected Scene scene;
+
+	public AbstractView(T controller, Stage stage, Scene scene) {
+		this.controller = controller;
+		this.stage = stage;
+		this.scene = scene;
+	}
+
+	@Override
+	public void show() {
+		setStyleClasses();
+		registerListener();
+		addPicturesToButtons();
+		fillComponentsWithSelectedLanguage();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@Override
+	public void pictureForButton(Image icon, Button button) {
+		button.setGraphic(new ImageView(icon));
+		button.setContentDisplay(ContentDisplay.TOP);
+	}
+
+	@Override
+	public void fillComponentWithText(Labeled labeled, String nameInBundle) {
+		// stuff to do
+	}
+
+}
