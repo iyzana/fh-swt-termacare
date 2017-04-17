e5599ea7c9481bc1ba8d55871c21e3cdb18c1de0
5he1d0r <sebastian.linde@adesso.de>
2017-04-17 14:23:44 +0200

    impl MedicationType

--- src/main/java/de/adesso/termacare/entity/MedicationType.java
+++ src/main/java/de/adesso/termacare/entity/MedicationType.java
@@ -1,3 +1,17 @@
 package de.adesso.termacare.entity;
 
-public enum MedicationType{}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public enum MedicationType{
+
+	INSPECTION(0L,"inspection"),
+	OPERATION(1L,"operation");
+
+	private long id;
+	private String type;
+}
