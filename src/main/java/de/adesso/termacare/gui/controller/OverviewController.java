14c741d74f5b504a8894af00ec628981e7743803
5he1d0r <sebastian.linde@adesso.de>
2017-04-30 15:39:04 +0200

    impl table of patients

--- src/main/java/de/adesso/termacare/gui/controller/OverviewController.java
+++ src/main/java/de/adesso/termacare/gui/controller/OverviewController.java
@@ -1,14 +1,71 @@
 package de.adesso.termacare.gui.controller;
 
+import de.adesso.termacare.data.dao.DAOPatient;
 import de.adesso.termacare.gui.construct.AbstractController;
 import de.adesso.termacare.gui.view.Overview;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class OverviewController extends AbstractController<Overview>{
 
+//	private PatientRepo repo = new PatientRepo();
+
 	@Override
 	public void init(Stage stage, Scene scene){
 		super.init(new Overview(this, stage, scene));
+		this.stage = stage;
+		this.scene = view.getScene();
+		fillTableWithColumns();
+		loadPersonsToTable();
+		checkButtons();
+	}
+
+	private void fillTableWithColumns(){
+		generateColumnFor("gender", 0, 50);
+		generateColumnFor("givenName", 100, 0);
+		generateColumnFor("familyName", 100, 0);
+		generateColumnFor("livingPostcode");
+		generateColumnFor("livingAddress");
+		generateColumnFor("billingPostcode");
+		generateColumnFor("billingAddress");
+	}
+
+	private void generateColumnFor(String identifier){
+		generateColumnFor(identifier, 0, 0);
+	}
+
+	private void generateColumnFor(String identifier, int minWidth, int maxWidth){
+		TableColumn<DAOPatient, String> column = new TableColumn<>(identifier);
+		if(minWidth != 0) column.setMinWidth(minWidth);
+		if(maxWidth != 0) column.setMaxWidth(maxWidth);
+		column.setCellValueFactory(new PropertyValueFactory<>(identifier));
+		view.getPatientTableView().getColumns().add(column);
+	}
+
+	private void loadPersonsToTable(){
+//		view.setPatients(
+//						FXCollections.observableArrayList(repo.list().stream().map(Patient::toDAO).collect(Collectors.toList())));
+	}
+
+	private void checkButtons(){
+		DAOPatient focusedItem = view.getPatientTableView().getFocusModel().getFocusedItem();
+
+	}
+
+	public void newPatient(){
+		PatientEditController patientEditController = new PatientEditController();
+	}
+
+	public void editPatient(){
+		DAOPatient focusedItem = view.getPatientTableView().getFocusModel().getFocusedItem();
+		PatientEditController patientEditController = new PatientEditController(focusedItem);
+		patientEditController.init(stage, scene);
+		patientEditController.show();
+	}
+
+	public void deletePatient(){
+
 	}
 }
--- src/main/java/de/adesso/termacare/gui/view/Overview.java
+++ src/main/java/de/adesso/termacare/gui/view/Overview.java
@@ -1,11 +1,29 @@
 package de.adesso.termacare.gui.view;
 
+import de.adesso.termacare.data.dao.DAOPatient;
 import de.adesso.termacare.gui.construct.AbstractView;
 import de.adesso.termacare.gui.controller.OverviewController;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class Overview extends AbstractView<OverviewController>{
+
+	private BorderPane pane = new BorderPane();
+	private ObservableList<DAOPatient> patients;
+	private TableView<DAOPatient> patientTableView = new TableView<>(patients);
+	private	Button newPatient = new Button("new");
+	private	Button editPatient = new Button("edit");
+	private	Button deletePatient = new Button("delete");
+	private HBox bottomBox = new HBox();
 
 	public Overview(OverviewController controller, Stage stage, Scene scene){
 		super(controller, stage, scene);
@@ -13,7 +31,13 @@ public class Overview extends AbstractView<OverviewController>{
 
 	@Override
 	public void init(){
+		bottomBox.getChildren().addAll(newPatient, editPatient, deletePatient);
 
+		pane.setCenter(patientTableView);
+		pane.setBottom(bottomBox);
+
+		scene = new Scene(pane);
+		scene.getStylesheets().add("main.css");
 	}
 
 	@Override
@@ -23,16 +47,23 @@ public class Overview extends AbstractView<OverviewController>{
 
 	@Override
 	public void fillComponentsWithSelectedLanguage(){
-
+		fillComponentWithText(newPatient, "newPatient");
+		fillComponentWithText(editPatient, "editPatient");
+		fillComponentWithText(deletePatient, "deletePatient");
 	}
 
 	@Override
 	public void registerListener(){
-
+		newPatient.setOnMouseClicked(event -> controller.newPatient());
+		editPatient.setOnMouseClicked(event -> controller.editPatient());
+		deletePatient.setOnMouseClicked(event -> controller.deletePatient());
 	}
 
 	@Override
 	public void setStyleClasses(){
-
+		newPatient.setId("newPatient");
+		editPatient.setId("editPatient");
+		deletePatient.setId("deletePatient");
+		patientTableView.setId("patientList");
 	}
 }
