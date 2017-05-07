bc0e58715fa6dbbb09fb2b73856afbd662c2eef2
succcubbus <jannis.kaiser2@gmail.com>
2017-05-08 00:09:57 +0200

    do import for Logger

--- src/main/java/de/adesso/termacare/TerMa.java
+++ src/main/java/de/adesso/termacare/TerMa.java
@@ -3,11 +3,12 @@ package de.adesso.termacare;
 import de.adesso.termacare.gui.controller.OverviewController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TerMa extends Application {
 
-    public static org.slf4j.Logger logger = LoggerFactory.getLogger(TerMa.class);
+    public static Logger logger = LoggerFactory.getLogger(TerMa.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception {
